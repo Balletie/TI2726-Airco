3652cb9045f2575cc03e4e11ada56f5f97b8aceb
Thomas Baars <T.C.Baars@student.tudelft.nl>
2014-10-21 00:32:09 +0200

    Added skeleton of behavioral description. Unsure how we should manage reset.

--- airco_I/airco_fsm.vhd
+++ airco_I/airco_fsm.vhd
@@ -17,9 +17,45 @@ entity airco_fsm is
 end airco_fsm;
 
 architecture behaviour of airco_fsm is
+	type airco_fsm_state is (OFF, HEAT, COOL, SERVICE);
+	signal state, new_state: airco_fsm_state;
 begin
--- ??
--- ??
+	lb1: process(clk)
+	begin
+		if (clk'event and clk = '1') then
+			state <= new_state;
+		end if;
+	end process;
+	
+	
+	//define ZEWG as a single signal?
+	//reset should show in the FSM diagram?
+	lb2: process(state, Z, E, W, G, c0, c1, reset?)
+	begin
+		case state is
+		when OFF = >
+			if() then
+			else
+			end if;
+			
+		when HEAT = >
+			if() then
+			else
+			end if;
+			
+		when COOL = >
+			if() then
+			else
+			end if;
+			
+		when SERVICE = >
+			if() then
+			else
+			end if;
+		
+			state <= new_state;
+		end case;
+	end process;
 end behaviour;
 
 
