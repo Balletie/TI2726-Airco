944d14f406a744f6f369f7b89b415e7773d3e0fd
Thomas Baars <T.C.Baars@student.tudelft.nl>
2014-10-22 00:14:33 +0200

    Added add_comp

--- airco_I/airco_comp.vhd
+++ airco_I/airco_comp.vhd
@@ -98,14 +98,40 @@ ENTITY add_comp IS
 END add_comp;
 
 ARCHITECTURE structural OF add_comp IS
+	COMPONENT inver 
+		PORT (a: in  std_logic;
+			y: out std_logic);
+	END COMPONENT;
+	
+	COMPONENT and_2
+		PORT (a, b: in  std_logic;
+			y:    out std_logic);
+	END COMPONENT;
 
+	COMPONENT or_2
+		PORT (a, b: in  std_logic;
+			y:    out std_logic);
+	END COMPONENT;
 
+	COMPONENT eight_bitadder
+		PORT (
+			A, B: in std_logic_vector(7 downto 0);
+			f: in std_logic;
+			C: inout std_logic_vector(7 downto 0);
+			o, z: out std_logic
+		);
+	END COMPONENT
 
+	signal bplusc, D : std_logic_vector(7 downto 0);
+	signal v0, n0, v1, n1, y: std_logic;
+	
 BEGIN
 
--- ??
--- ??
--- ??
+	lbl0: eight_bitadder PORT MAP (A=>B, B=>C, f='0', C=>bplusc, o=>v0, z=>n0);
+	lbl1: eight_bitadder PORT MAP (A=>A, B=>bplusc, f='1', C=>D, o=>v1, z=>n1);
+	lbl2: inver PORT MAP (a=>D(7), y=>y);
+	lbl3: and_2 PORT MAP (a=>y, b=>n1, y=>gz);
+	lbl4: or_2 PORT MAP (a=>v0, b=>v1, y=>o);
 
 END structural;
 
@@ -129,10 +155,7 @@ architecture structural of airco_comp is
 
 BEGIN
 
--- ??
--- ??
--- ??
--- ??
+
 
 END structural;
 
